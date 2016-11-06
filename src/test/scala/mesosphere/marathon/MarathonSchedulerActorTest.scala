d66db4f8674db03741b72ba0d95c1c46f310f175
Aleksey Dukhovniy <adukhovniy@mesosphere.io>
2016-11-06 15:24:07 +0100

    Fix MarathonSchedulerActor "Cancellation timeout" test

--- src/test/scala/mesosphere/marathon/MarathonSchedulerActorTest.scala
+++ src/test/scala/mesosphere/marathon/MarathonSchedulerActorTest.scala
@@ -510,8 +510,7 @@ class MarathonSchedulerActorTest extends MarathonActorSupport
     }
   }
 
-  // TODO: Fix  this test...
-  ignore("Cancellation timeout - this test is really racy and fails intermittently.") {
+  test("Cancellation timeout") {
     val f = new Fixture
     import f._
     val app = AppDefinition(id = PathId("app1"), cmd = Some("cmd"), instances = 2, upgradeStrategy = UpgradeStrategy(0.5))
@@ -524,10 +523,13 @@ class MarathonSchedulerActorTest extends MarathonActorSupport
     instanceTracker.specInstancesLaunchedSync(app.id) returns Seq.empty[Instance]
     appRepo.delete(app.id) returns Future.successful(Done)
 
-    val schedulerActor = TestActorRef[MarathonSchedulerActor](
+    // Dummy deployment manager to make sure, that it will not cancel the deployment.
+    val dummyProps: SchedulerActions => Props = schedulerActions => Props.empty
+
+    val schedulerActor = system.actorOf(
       MarathonSchedulerActor.props(
         schedulerActions,
-        deploymentManagerProps,
+        dummyProps,
         historyActorProps,
         deploymentRepo,
         hcManager,
@@ -535,25 +537,22 @@ class MarathonSchedulerActorTest extends MarathonActorSupport
         queue,
         holder,
         electionService,
-        system.eventStream
+        system.eventStream,
+        1.milliseconds
       )
     )
+
     try {
-      val probe = TestProbe()
-      schedulerActor.tell(LocalLeadershipEvent.ElectedAsLeader, probe.testActor)
-      schedulerActor.tell(Deploy(plan), probe.testActor)
+      schedulerActor ! LocalLeadershipEvent.ElectedAsLeader
+      schedulerActor ! Deploy(plan)
 
-      probe.expectMsgType[DeploymentStarted]
+      expectMsgType[DeploymentStarted]
 
-      schedulerActor.tell(Deploy(plan, force = true), probe.testActor)
+      schedulerActor ! Deploy(plan, force = true)
 
-      val answer = probe.expectMsgType[CommandFailed]
-
+      val answer = expectMsgType[CommandFailed]
       answer.reason.isInstanceOf[TimeoutException] should be(true)
       answer.reason.getMessage should be
-
-      // this test has more messages sometimes!
-      // needs: probe.expectNoMsg()
     } finally {
       stopActor(schedulerActor)
     }
